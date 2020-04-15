e3f451145d905c3c5cb32e0ab9983dad6131d6e1
Soufiane Berouel <soufiane.berouel@gmail.com>
2020-04-15 16:10:26 -0500

    first commit - simple SpringBoot app

---
+++ src/main/java/com/poc/test/spring/boot/actuator/SpringBootApplicationStarter.java
@@ -0,0 +1,11 @@
+package com.poc.test.spring.boot.actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApplicationStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApplicationStarter.class);
+    }
+}
