d3e1c9513edd0c7d7639b90ce9039a9f61d5f86d
Soufiane Berouel <soufiane.berouel@gmail.com>
2020-04-15 17:52:58 -0500

    override health api to point to customized health check method

---
+++ src/main/java/com/poc/test/spring/boot/actuator/myhealth/MyHealthCheck.java
@@ -0,0 +1,33 @@
+package com.poc.test.spring.boot.actuator.myhealth;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.boot.actuate.health.Status;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.io.File;
+
+@Component
+public class MyHealthCheck implements HealthIndicator {
+
+    @Value("${probe.file.path}")
+    private String probeFilePath;
+
+    private File probeFile;
+
+    @PostConstruct
+    private void init(){
+        probeFile = new File(probeFilePath);
+    }
+
+    @Override
+    public Health health() {
+        if (!probeFile.exists()){
+            return Health.down().build();
+        }
+        return Health.up().build();
+    }
+}
